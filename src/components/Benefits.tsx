0ba40de20e2ddcfd742a0303b3c40e184fdb3142
Raul <rauleburro@gmail.com>
2024-11-18 00:21:20 +0100

    Updated poster structure

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-import { Container }  from "@/components/Container";
+import React, { useState } from "react";
+import { Container } from "@/components/Container";
 
 interface BenefitsProps {
   imgPos?: "left" | "right";
+  videoId?: string;
   data: {
     imgPos?: "left" | "right";
     title: string;
@@ -17,70 +19,93 @@ interface BenefitsProps {
   };
 }
 export const Benefits = (props: Readonly<BenefitsProps>) => {
-  const { data } = props;
+  const [playVideo, setPlayVideo] = useState(false);
+  const { data, videoId = "" } = props;
   return (
-      <Container className="flex flex-wrap mb-20 lg:gap-10 lg:flex-nowrap ">
-        <div
-          className={`flex items-center justify-center w-full lg:w-1/2 ${
-            props.imgPos === "right" ? "lg:order-1" : ""
-          }`}>
-          <div>
-            <Image
-              src={data.image}
-              width={521}
-              height={521}
-              alt="Benefits"
-              className={"object-cover"}
-              placeholder="blur"
-              blurDataURL={data.image.src}
-            />
+    <Container className="flex flex-wrap mb-20 lg:gap-10 lg:flex-nowrap ">
+      <div
+        className={`flex items-center justify-center w-full lg:w-1/2 ${
+          props.imgPos === "right" ? "lg:order-1" : ""
+        }`}
+      >
+        <div className="relative w-full h-[500px] max-w-4xl mx-auto overflow-hidden lg:mb-20 rounded-2xl bg-indigo-300 cursor-pointer bg-gradient-to-tr from-purple-400 to-indigo-700">
+          {!playVideo && (
+            <button
+              onClick={() => setPlayVideo(!playVideo)}
+              className="absolute inset-auto w-16 h-16 text-white transform -translate-x-1/2 -translate-y-1/2 lg:w-28 lg:h-28 top-1/2 left-1/2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-16 h-16  lg:w-28 lg:h-28"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="sr-only">Play Video</span>
+            </button>
+          )}
+          {playVideo && (
+            <iframe
+              src={`https://www.youtube-nocookie.com/embed/${props.videoId}?controls=0&autoplay=1`}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="w-full h-full aspect-video"
+            ></iframe>
+          )}
+        </div>
+      </div>
+
+      <div
+        className={`flex flex-wrap w-full lg:w-1/2 ${
+          data.imgPos === "right" ? "lg:justify-end" : ""
+        }`}
+      >
+        <div>
+          <div className="flex flex-col w-full mt-4">
+            <h3 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
+              {data.title}
+            </h3>
+
+            <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
+              {data.desc}
+            </p>
+          </div>
+
+          <div className="w-full mt-5">
+            {data.bullets.map((item, index) => (
+              <Benefit key={index} title={item.title} icon={item.icon}>
+                {item.desc}
+              </Benefit>
+            ))}
           </div>
         </div>
-
-        <div
-          className={`flex flex-wrap items-center w-full lg:w-1/2 ${
-            data.imgPos === "right" ? "lg:justify-end" : ""
-          }`}>
-          <div>
-            <div className="flex flex-col w-full mt-4">
-              <h3 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
-                {data.title}
-              </h3>
-
-              <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
-                {data.desc}
-              </p>
-            </div>
-
-            <div className="w-full mt-5">
-              {data.bullets.map((item, index) => (
-                <Benefit key={index} title={item.title} icon={item.icon}>
-                  {item.desc}
-                </Benefit>
-              ))}
-            </div>
-          </div>
-        </div>
-      </Container>
+      </div>
+    </Container>
   );
 };
 
 function Benefit(props: any) {
   return (
-      <div className="flex items-start mt-8 space-x-3">
-        <div className="flex items-center justify-center flex-shrink-0 mt-1 bg-indigo-500 rounded-md w-11 h-11 ">
-          {React.cloneElement(props.icon, {
-            className: "w-7 h-7 text-indigo-50",
-          })}
-        </div>
-        <div>
-          <h4 className="text-xl font-medium text-gray-800 dark:text-gray-200">
-            {props.title}
-          </h4>
-          <p className="mt-1 text-gray-500 dark:text-gray-400">
-            {props.children}
-          </p>
-        </div>
+    <div className="flex items-start mt-8 space-x-3">
+      <div className="flex items-center justify-center flex-shrink-0 mt-1 bg-indigo-500 rounded-md w-11 h-11 ">
+        {React.cloneElement(props.icon, {
+          className: "w-7 h-7 text-indigo-50",
+        })}
       </div>
+      <div>
+        <h4 className="text-xl font-medium text-gray-800 dark:text-gray-200">
+          {props.title}
+        </h4>
+        <p className="mt-1 text-gray-500 dark:text-gray-400">
+          {props.children}
+        </p>
+      </div>
+    </div>
   );
 }
+``;
